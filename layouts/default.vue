1ccfd9a3ecaa33b081c75fe0c07304dc16a9d413
Leonardo Araujo Pimentel <pimentelleo@brq.com>
2022-11-17 17:11:04 -0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body" as="main">
+          <CReset />
+          <div>Header</div>
+          <Nuxt />
+          <div>Footer</div>
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+<script>
+import {
+  CThemeProvider,
+  Flex,
+  Spacer,
+  CColorModeProvider,
+  CReset,
+  CBox,
+} from '@chakra-ui/vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    CThemeProvider,
+    CColorModeProvider,
+    CReset,
+    CBox,
+  },
+}
+</script>
